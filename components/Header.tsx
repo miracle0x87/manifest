eb1899e0ead1178a8d00b46390f49f00213cbf06
Miracle <108835149+miracle0x87@users.noreply.github.com>
2025-07-23 21:43:55 +0500

    Add files via upload

---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header className="p-4 shadow-md bg-white dark:bg-black">
+      <div className="container mx-auto">
+        <h1 className="text-xl font-bold">Manifest</h1>
+      </div>
+    </header>
+  );
+}
